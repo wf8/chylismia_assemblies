59ecb533c5641e0813a3651fc7da2e93cb06f329
wf <willfreyman@gmail.com>
2016-10-23 15:19:23 -0700

    better filtering of reference assemblies

--- assemble_33.sh
+++ assemble_33.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-for index in 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 # 33
-do
+for index in 33
 do
 
     echo "Uncompressing index: $index"
     gunzip 150PE/Index_${index}_*R1*.fastq.gz
     gunzip 150PE/Index_${index}_*R2*.fastq.gz
+        
+    echo "Filtering adapter sequences..."
+    java -classpath utilities/Trimmomatic-0.36/trimmomatic-0.36.jar org.usadellab.trimmomatic.TrimmomaticPE -threads 4 -phred33 150PE/Index_${index}_*R1*.fastq 150PE/Index_${index}_*R2*.fastq ${index}_trimmed_P1.fq ${index}_trimmed_U1.fq ${index}_trimmed_P2.fq ${index}_trimmed_U2.fq ILLUMINACLIP:utilities/Fast-Plast/bin/NEB-PE.fa:1:30:10 SLIDINGWINDOW:10:20 MINLEN:40
 
     # exclude single copy loci with too low coverage: "PgiC" "LFY" "WAXY" "ADH"
     # make reference assemblies of nuclear rDNA and mitochondrial genes:
-    for ref in "ETS" "ITS" "26S" "18S" "CYTB" "MatR" "ATP6" "trnI" "ORF224" "ORF250" "ORF454" "NAD5" "NAD4" "NAD6" "NAD7" "RPS13" "COXIII" "ATP8" "ORF206" "RPS14" "NAD3" "NAD2" "S3" "trnS" "ATP9" "ATP1" "COXI"
-    
+    for ref in "CYTB" "PgiC" "LFY" "WAXY" "ADH" "26S" "18S" "MatR" "ATP6" "trnI" "ORF224" "ORF250" "ORF454" "NAD5" "NAD4" "NAD6" "NAD7" "RPS13" "COXIII" "ATP8" "ORF206" "RPS14" "NAD3" "NAD2" "S3" "trnS" "ATP9" "ATP1" "COXI"
+    do
+
         reference="reference_genomes/${ref}.fasta"
         mkdir assemblies/bwa/$ref
         output_dir="assemblies/bwa/${ref}/"
         
         echo "Assembling index: $index"
-        
-        echo "Filtering adapter sequences..."
-        java -classpath utilities/Trimmomatic-0.36/trimmomatic-0.36.jar org.usadellab.trimmomatic.TrimmomaticPE -threads 4 -phred33 150PE/Index_${index}_*R1*.fastq 150PE/Index_${index}_*R2*.fastq ${index}_trimmed_P1.fq ${index}_trimmed_U1.fq ${index}_trimmed_P2.fq ${index}_trimmed_U2.fq ILLUMINACLIP:utilities/Fast-Plast/bin/NEB-PE.fa:1:30:10 SLIDINGWINDOW:10:20 MINLEN:40
         
         echo "Mapping reads with bwa..."
         bwa mem $reference ${index}_trimmed_P1.fq ${index}_trimmed_P2.fq > ${index}.sam
@@ -33,18 +33,29 @@ do
         echo "Determining read depth..."
         samtools depth ${index}_sorted.bam > ${index}_read_depth.tsv
         
-        echo "Making BAM pileup, generating consensus FASTA sequence..."
-        samtools mpileup -uf $reference ${index}_sorted.bam | bcftools view -cg - | vcfutils.pl vcf2fq > ${index}.fastq
+        echo "Making BAM pileup, filtering by phred quality 20..."
+        samtools mpileup -Q 20 -Agf $reference ${index}_sorted.bam > 33.mpilup
+
+        echo "Generating consensus genotypes..."
+        bcftools view -cg ${index}.mpilup > temp.vcf
+        
+        echo "Filtering for read depth >= 20..."
+        python filter_vcf_read_depth.py temp.vcf 20
+
+        echo "Generating final FASTA sequence..."
+        vcfutils.pl vcf2fq temp.vcf.filtered > ${index}.fastq
         seqtk seq -A ${index}.fastq > ${index}.fasta
         
         echo "Cleaning up..."
-        rm ${index}_trimmed_*
+        rm temp.vcf temp.vcf.filtered
+        rm ${index}_sorted.bam ${index}.bam ${index}.sam
         mkdir ${output_dir}${index}
-        mv ${index}* ${output_dir}${index}
+        mv ${index}.fasta ${index}.fastq ${index}_read_depth.tsv ${output_dir}${index}
     
     done
 
     echo "Re-compressing index: $index"
+    rm ${index}_trimmed_*
     gzip 150PE/Index_${index}_*R1*.fastq
     gzip 150PE/Index_${index}_*R2*.fastq
 
