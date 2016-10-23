8cecc1fe4e25c6330ebf90649df05d716c006618
wf <willfreyman@gmail.com>
2016-10-23 00:17:43 -0700

    more rDNA and mitochondrial regions

--- assemble_other.sh
+++ assemble_other.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-for ref in "ETS" "ITS" "PgiC" "LFY" "WAXY" "ADH"
+for index in 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 # 33
 do
-    reference="reference_genomes/${ref}.fasta"
-    mkdir assemblies/bwa/$ref
-    output_dir="assemblies/bwa/${ref}/"
+do
 
-    for index in 33 #34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58
-    do
+    echo "Uncompressing index: $index"
+    gunzip 150PE/Index_${index}_*R1*.fastq.gz
+    gunzip 150PE/Index_${index}_*R2*.fastq.gz
 
-        echo "Uncompressing index: $index"
-        gunzip 150PE/Index_${index}_*R1*.fastq.gz
-        gunzip 150PE/Index_${index}_*R2*.fastq.gz
+    # exclude single copy loci with too low coverage: "PgiC" "LFY" "WAXY" "ADH"
+    # make reference assemblies of nuclear rDNA and mitochondrial genes:
+    for ref in "ETS" "ITS" "26S" "18S" "CYTB" "MatR" "ATP6" "trnI" "ORF224" "ORF250" "ORF454" "NAD5" "NAD4" "NAD6" "NAD7" "RPS13" "COXIII" "ATP8" "ORF206" "RPS14" "NAD3" "NAD2" "S3" "trnS" "ATP9" "ATP1" "COXI"
+    
+        reference="reference_genomes/${ref}.fasta"
+        mkdir assemblies/bwa/$ref
+        output_dir="assemblies/bwa/${ref}/"
         
         echo "Assembling index: $index"
         
@@ -38,12 +41,13 @@ do
         rm ${index}_trimmed_*
         mkdir ${output_dir}${index}
         mv ${index}* ${output_dir}${index}
-        
-        echo "Re-compressing index: $index"
-        gzip 150PE/Index_${index}_*R1*.fastq
-        gzip 150PE/Index_${index}_*R2*.fastq
-
+    
     done
+
+    echo "Re-compressing index: $index"
+    gzip 150PE/Index_${index}_*R1*.fastq
+    gzip 150PE/Index_${index}_*R2*.fastq
+
 done
 
 # Illumina adapter sequences were removed and the raw sequence reads were quality filtered using Trimmomatic v0.36 (CITE). Reads were trimmed when the average phred quality score in a 10-bp sliding window was less than 20. Reads that were less than 40 bp or that did not survive the filtering process in both forward and reverse directions were excluded.
--- reference_genomes/make_indices.sh
+++ reference_genomes/make_indices.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-for seq in "ADH.fasta" "ETS.fasta" "ITS.fasta" "PgiC.fasta" "LFY.fasta" "WAXY.fasta"
+#for seq in "ADH.fasta" "ETS.fasta" "ITS.fasta" "PgiC.fasta" "LFY.fasta" "WAXY.fasta"
+for seq in "ATP6" "trnI" "ORF224" "ORF250" "ORF454" "NAD5" "NAD4" "NAD6" "NAD7" "RPS13" "COXIII" "ATP8" "ORF206" "RPS14" "NAD3" "NAD2" "S3" "trnS" "MatR" "ATP9" "ATP1" "COXI" "CYTB" "26S" "18S"
 do
-    bwa index $seq
+    bwa index ${seq}.fasta
 done
 
